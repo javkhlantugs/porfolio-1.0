708ed7573eb0aab12350c67747f1bbc01caa998f
javkhlantugs <jay.mudgil@gmail.com>
2017-03-28 03:21:55 -0700

    working portfolio animation

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,10 @@
 $(document).on('turbolinks:load', function() {
-	$('.my-container').css('height', minimumHeight())
-	$('.avatar-container').css('height', aboutHeight())
-	$('.js-developer').click(adjustWidthDev())
-	})
+	$('.my-container').css('height', minimumHeight());
+	$('.avatar-container').css('height', aboutHeight());
+	$('.window-inactive').css('height', windowsHeight() * 0.4);
+	$('.developer-button').on('click', adjustWidthDev);
+	$('.designer-button').on('click', adjustWidthDes);
+});
 
 
 // windows height
@@ -13,7 +15,7 @@ var windowsHeight = function () {
 
 function minimumHeight() {
 	console.log(windowsHeight())
-	var halfHeight = windowsHeight() * 0.5
+	var halfHeight = windowsHeight() * 0.6
 	if (halfHeight < 300) {
 		return 370 
 	} else {
@@ -33,6 +35,38 @@ function aboutHeight() {
 
 // about height end
 
-// developer window
+//portfolio animation
+function adjustWidthDev() {
+	if (checkDesignerWindow()){
+		$('.js-designer').toggleClass('designer-min designer-window');
+		$('.js-developer').toggleClass('developer-min developer-window');
+	} else {
+		$('.js-developer').toggleClass('window-active window-inactive developer-window developer-window-inactive');
+		$('.js-designer').toggleClass('window-active window-inactive designer-min');
+	}
+}
 
+function adjustWidthDes() {
+	if (checkDeveloperWindow()){
+		$('.js-developer').toggleClass('developer-min developer-window');
+		$('.js-designer').toggleClass('designer-min designer-window');
+	} else {
+		$('.js-designer').toggleClass('window-active window-inactive designer-window designer-window-inactive')
+		$('.js-developer').toggleClass('window-active window-inactive developer-min')
+	}
+}
 
+function checkDesigner() {
+	return $('.js-designer').hasClass('window-active');
+}
+function checkDeveloper() {
+	return $('.js-developer').hasClass('window-active');
+}
+
+function checkDesignerWindow() {
+	return $('.js-designer').hasClass('designer-window');
+}
+
+function checkDeveloperWindow() {
+	return $('.js-developer').hasClass('developer-window');
+}
